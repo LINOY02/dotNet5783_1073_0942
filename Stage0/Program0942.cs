12f4f383e70583b361f329531f71356ccd8e80d9
LINOY <linoyyaday@gmail.com>
2022-10-27 16:47:39 +0300

    Initial code + Welcome function

---
+++ Stage0/Program0942.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Stage0 // Note: actual namespace depends on the project name.
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0942();
+            Welcome1073();
+            Console.ReadKey();
+        }
+        static partial void Welcome1073();
+        private static void Welcome0942()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console appliction", name);
+        }
+    }
+}
