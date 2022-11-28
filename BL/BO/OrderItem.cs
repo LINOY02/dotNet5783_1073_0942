38a6407c4c143de8daec439c6b70e35641164eaa
Tamar Gefner <tgwork640@gmail.com>
2022-11-28 18:23:32 +0200

    Add to BlApi interface IProduct

--- BL/BO/OrderItem.cs
+++ BL/BO/OrderItem.cs
@@ -5,9 +5,12 @@ public class OrderItem
 {
     public int ID { get; set; }
     public string Name { get; set; }
-
     public int ProductID { get; set; }
     public double Price { get; set; }
     public int Amount { get; set; }
     public double TotalPrice { get; set; }
+    public override string ToString()
+    {
+        return this.ToStringProperty();
+    }
 }
---
+++ BL/BlApi/IBl.cs
@@ -0,0 +1,22 @@
+﻿
+namespace BlApi;
+/// <summary>
+/// A main interface that centers all the interfaces of the logical layer
+/// </summary>
+public interface IBl
+{
+    /// <summary>
+    /// A variable to represent a product's interface
+    /// </summary>
+    public IProduct Product { get;}
+
+    /// <summary>
+    /// A variable to represent an order's interface
+    /// </summary>
+    public IOrder Order { get; }
+
+    /// <summary>
+    /// A variable to represent a cart's interface
+    /// </summary>
+    public ICart Cart { get; }
+}
