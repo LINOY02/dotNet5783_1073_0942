89f6d53884c086887fff856bafcd707b583eb2e2
LINOY <linoyyaday@gmail.com>
2022-11-28 16:49:10 +0200

    add classes to a BO file

---
+++ BL/BO/Order.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace BO;
+
+public class Order
+{
+    public int ID { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerEmail { get; set; }
+    public string CustomerAdress { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime ShipDate { get; set; }
+    public DateTime DeliveryDate { get; set; }
+    public OrderStatus Status { get; set; }
+    public IEnumerable<OrderItem> Items { get; set; }
+    public double TotalPrice { get; set; }
+    public override string ToString()
+    {
+        return this.ToStringProperty();
+    }
+}
---
+++ BL/BO/OrderTracking.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace BO;
+
+public class OrderTracking
+{
+    public int ID { get; set; }
+    public OrderStatus status { get; set; }
+    public override string ToString()
+    {
+        return this.ToStringProperty();
+    }
+}
